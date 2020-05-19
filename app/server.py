0bd143eb4e7d8c98fc25d26c3ec2c2428fffb502
Vignesh Kumar <Vigneshthanga@users.noreply.github.com>
2020-05-18 19:56:04 -0700

    Add files via upload

--- app/server.py
+++ app/server.py
@@ -8,11 +8,24 @@ from starlette.applications import Starlette
 from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
+import os
+import warnings
+import matplotlib
+matplotlib.use('Agg')
+from matplotlib import figure
+from matplotlib.backends import backend_agg
+import numpy as np
+import tensorflow_probability as tfp
+import tensorflow as tf
+import cv2
+import h5py
+
+warnings.simplefilter(action='ignore')
 
 export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
 export_file_name = 'export.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['0', '1', '2', '3', '4']
 path = Path(__file__).parent
 
 app = Starlette()
@@ -43,10 +56,115 @@ async def setup_learner():
             raise
 
 
+'''
 loop = asyncio.get_event_loop()
 tasks = [asyncio.ensure_future(setup_learner())]
 learn = loop.run_until_complete(asyncio.gather(*tasks))[0]
 loop.close()
+'''
+try:
+  import seaborn as sns  # pylint: disable=g-import-not-at-top
+  HAS_SEABORN = True
+except ImportError:
+  HAS_SEABORN = False
+
+tfd = tfp.distributions
+
+IMAGE_SHAPE = [224, 224, 3]
+NUM_TRAIN_EXAMPLES = 2782
+NUM_CLASSES = 5
+
+learning_rate = 0.001
+num_epochs = 10
+batch_size = 32
+data_dir = '/content/gdrive/My Drive/bayesian_eye/'
+model_dir = '/content/gdrive/My Drive/bayesian_eye/model_v1'
+viz_steps = 10
+fake_data = False
+num_monte_carlo = 50
+
+def create_model():
+  # KL divergence weighted by the number of training samples, using
+  # lambda function to pass as input to the kernel_divergence_fn on
+  # flipout layers.
+
+  kl_divergence_function = (lambda q, p, _: tfd.kl_divergence(q, p) /
+                            tf.cast(NUM_TRAIN_EXAMPLES, dtype=tf.float32))
+
+  model = tf.keras.models.Sequential(
+      [
+      tfp.layers.Convolution2DFlipout(
+          32, kernel_size=5, padding='SAME',
+          kernel_divergence_fn=kl_divergence_function,
+          activation=tf.nn.relu),
+      tf.keras.layers.MaxPooling2D(
+          pool_size=[2, 2], strides=[2, 2],
+          padding='SAME'),
+      tfp.layers.Convolution2DFlipout(
+          64, kernel_size=5, padding='SAME',
+          kernel_divergence_fn=kl_divergence_function,
+          activation=tf.nn.relu),
+      tf.keras.layers.MaxPooling2D(
+          pool_size=[2, 2], strides=[2, 2],
+          padding='SAME'),
+      tfp.layers.Convolution2DFlipout(
+          128, kernel_size=5, padding='SAME',
+          kernel_divergence_fn=kl_divergence_function,
+          activation=tf.nn.relu),
+      tf.keras.layers.Flatten(),
+      tfp.layers.DenseFlipout(
+          300, kernel_divergence_fn=kl_divergence_function,
+          activation=tf.nn.relu),
+      tfp.layers.DenseFlipout(
+          100, kernel_divergence_fn=kl_divergence_function,
+          activation=tf.nn.relu),
+      tfp.layers.DenseFlipout(
+          NUM_CLASSES, kernel_divergence_fn=kl_divergence_function,
+          activation=tf.nn.softmax)
+  ]
+  )
+  optimizer = tf.keras.optimizers.Adam(lr=learning_rate)
+
+  # We use the categorical_crossentropy loss since the Eye dataset contains
+  # five labels. The Keras API will then automatically add the
+  # Kullback-Leibler divergence (contained on the individual layers of
+  # the model), to the cross entropy loss, effectively
+  # calcuating the (negated) Evidence Lower Bound Loss (ELBO)
+
+  model.compile(optimizer, loss='categorical_crossentropy',
+                metrics=['accuracy'], experimental_run_tf_function=False)
+  return model
+
+model = create_model()
+model.build(input_shape=[None, 224, 224, 3])
+
+file = h5py.File('bayesianmodel.h5', 'r')
+weight = []
+for i in range(len(file.keys())):
+   weight.append(file['weight' + str(i)][:])
+model.set_weights(weight)
+
+def crop_image_from_gray(img,tol=7):
+    if img.ndim ==2:
+        mask = img>tol
+        return img[np.ix_(mask.any(1),mask.any(0))]
+    elif img.ndim==3:
+        gray_img = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+        mask = gray_img>tol
+
+        check_shape = img[:,:,0][np.ix_(mask.any(1),mask.any(0))].shape[0]
+        if (check_shape == 0): # image is too dark so that we crop out everything,
+            return img # return original image
+        else:
+            img1=img[:,:,0][np.ix_(mask.any(1),mask.any(0))]
+            img2=img[:,:,1][np.ix_(mask.any(1),mask.any(0))]
+            img3=img[:,:,2][np.ix_(mask.any(1),mask.any(0))]
+    #         print(img1.shape,img2.shape,img3.shape)
+            img = np.stack([img1,img2,img3],axis=-1)
+    #         print(img.shape)
+        return img
+
+
 
 
 @app.route('/')
@@ -59,9 +177,17 @@ async def homepage(request):
 async def analyze(request):
     img_data = await request.form()
     img_bytes = await (img_data['file'].read())
-    img = open_image(BytesIO(img_bytes))
-    prediction = learn.predict(img)[0]
-    return JSONResponse({'result': str(prediction)})
+    decoded = cv2.imdecode(np.frombuffer(img_bytes, np.uint8), -1)
+    image = cv2.cvtColor(decoded,cv2.COLOR_BGR2RGB)
+    image = crop_image_from_gray(image)
+    image = cv2.resize(image, (224, 224))
+    image=cv2.addWeighted ( image,4, cv2.GaussianBlur( image , (0,0) , 10) ,-4 ,128)
+    image = image.reshape(1,224,224,3)
+    image = image/255.0
+    probs = model.predict(image)
+    res = np.argmax(probs)
+    conf = np.max(probs)
+    return JSONResponse({'result': str(res), 'conf' : str(conf)})
 
 
 if __name__ == '__main__':
